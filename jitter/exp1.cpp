9edb064ab69649e73dbebfd97880ec5f6187723e
Alfred Bratterud <alfred@includeos.org>
2019-03-18 13:21:00 -0400

    Preallocate cycle counters vector

--- jitter/exp1.cpp
+++ jitter/exp1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstdint>
+
 
 using namespace std;
 
@@ -19,7 +21,11 @@ std::vector<uint64_t> cycle_counter;
 
 
 std::vector<uint64_t>& experiment(int task_size = 1000, int task_count = 1000) {
-
+  
+  // Reserve memory for counters in advance
+  cycle_counter.reserve(task_count * sizeof(decltype(cycle_counter)::value_type));
+  
+  // Execute task_count tasks of task_size  
   for (int i = 0; i < task_count; i++) {
     auto t1 = cycles();
     task(task_size);
