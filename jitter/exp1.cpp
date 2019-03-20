e8521718ab75ff5106baa5c515c4d598fa27c0f1
Magnus Skjegstad <magnus@skjegstad.com>
2019-03-20 11:58:22 +0100

    Reserve memory before using the cycle_count vector
    Signed-off-by: Magnus Skjegstad <magnus@skjegstad.com>

--- jitter/exp1.cpp
+++ jitter/exp1.cpp
@@ -19,18 +19,17 @@ inline uint64_t cycles() noexcept {
 
 std::vector<uint64_t> cycle_counter;
 
-
 std::vector<uint64_t>& experiment(int task_size = 1000, int task_count = 1000) {
   
   // Reserve memory for counters in advance
-  cycle_counter.reserve(task_count * sizeof(decltype(cycle_counter)::value_type));
+  cycle_counter.resize(task_count, 0);
   
   // Execute task_count tasks of task_size  
   for (int i = 0; i < task_count; i++) {
     auto t1 = cycles();
     task(task_size);
     auto t2 = cycles();
-    cycle_counter.push_back(t2 - t1);
+    cycle_counter[i] = t2 - t1;
   }
 
   return cycle_counter;
