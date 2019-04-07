363dcf19ae1a4dbd04a3b6ac9e8e7748f03d7c34
Alfred Bratterud <alfred@includeos.org>
2019-04-07 15:11:12 +0200

    Added assembly-task etc.

--- jitter/exp.hpp
+++ jitter/exp.hpp
@@ -1,9 +1,9 @@
 
-__attribute__((noinline))
-void task(int iterations) {
+inline void task(int iterations) {
   volatile int i = 0;
   while(i < iterations) i++;
 }
+
 
 // From recommendations in Section 3.2.1 in
 // https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
@@ -26,4 +26,17 @@ inline uint64_t cycles_end() noexcept {
                 "CPUID\n\t": "=r" (cycles_high), "=r" (cycles_low)::
                 "%rax", "%rbx", "%rcx", "%rdx");
   return ((uint64_t) cycles_low) | ((uint64_t) cycles_high) << 32;
+}
+
+
+#define ASM_TASK_SIZE "20"
+#define STR(A) #A
+
+inline void task_asm() {
+  asm volatile(
+    "mov $" ASM_TASK_SIZE ", %%rsi\n"
+    ".task_asm_loop:\n"
+    "dec %%rsi\n"
+    "jnz .task_asm_loop\n"
+    ::: "%rsi");
 }
